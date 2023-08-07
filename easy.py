277585867dd5dec9a97c72efbbea3fb93aa59a3a
lzwiel <117366092+lzwiel@users.noreply.github.com>
2023-08-07 16:00:28 +0800

    Update easy.py

--- easy.py
+++ easy.py
@@ -6,24 +6,30 @@ numbers = [int(num) for num in numbers]
 
 # 求和
 sum_of_numbers = sum(numbers)
-print("求和结果:", sum_of_numbers)
+print("Sum:", sum_of_numbers)
 
 # 计数
 count_of_numbers = len(numbers)
-print("计数结果:", count_of_numbers)
+print("Count:", count_of_numbers)
 
 # 求平均数
-average = sum_of_numbers / count_of_numbers
-print("平均数:", average)
-
-# 求中位数
-median = statistics.median(numbers)
-print("中位数:", median)
+average_of_numbers = sum_of_numbers / count_of_numbers
+print("Average:", average_of_numbers)
 
 # 求众数
-mode = statistics.mode(numbers)
-print("众数:", mode)
+mode_of_numbers = statistics.mode(numbers)
+print("Mode:", mode_of_numbers)
 
-# 求方差
-variance = statistics.variance(numbers)
-print("方差:", variance)
+# 求中位数
+median_of_numbers = statistics.median(numbers)
+print("Median:", median_of_numbers)
+
+# 找出最大数和最小数
+max_number = max(numbers)
+min_number = min(numbers)
+print("Max:", max_number)
+print("Min:", min_number)
+
+# 重新按数字大小从大到小重新排序
+sorted_numbers = sorted(numbers, reverse=True)
+print("Sorted:", sorted_numbers)
